ddf64f840697df3bba28de716a1d54e022dc6744
victor5536 <joaovictornunesresende7@gmail.com>
2025-04-10 20:21:13 -0300

    aula 10-04

---
+++ area_restrita/index.php
@@ -0,0 +1,16 @@
+<?php header("location: login.php"); ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" href="css/estilos.css" />
+    <title>Area Restrita</title>
+</head>
+<body>
+    <div id="interface">
+
+    </div>
+</body>
+</html>
